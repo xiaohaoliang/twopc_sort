b020637a1fa8a13bf7e187ffce3291579a3b0aee
xiaohaoliang <xiaohaoliang@outlook.com>
2018-03-17 12:07:42 +0800

    init planA

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # twopc_sort
+
+实现 twopc_sort 的 sort 函数。
+sort 函数功能定义如下
+假设 dataStreamings 的数据是无尽的，即有无穷个 data 会产生；为了方便运行代码中设置了最大消息个数，但是假设的前提不变，必须满足！
+按照 commit 值的大小对所有 kind 为 commit 的数据进行**排序并且输出**
+
+实现目标：
+请用你能想到**最快的排序**方式实现 sort 代码
+请为 sort 代码部分实现**流控**
+
+# 思路
+
+goroutine 循环逻辑
+```
+ 1. generate prepare
+ 2. sleep 0~5 ms
+ 3. send prepare msg to channel
+ 4. sleep 0~5 ms
+ 5. generate commit
+ 6. sleep 0~5 ms
+ 7. send commit msg to channel
+ 8. sleep 0~50ms --> 1
+```
+
+分析code得到以下特征： 
+- 对于同一个channel里面的msg的sendTime总是递增的
+- sendTime大于1005ms的msg的commit 总是大于 sendTime=1000ms的msg的commit (方案一的排序窗口)
+- TODO 方案二	
+
+## 方案一 
+使用了golang的sort库和rate库
+
+- 对每个chan的输出的msg(过滤掉prepare消息)按照5ms分块
+- 块内用sort按照commit排序， 
+- 相邻两个块，归并输出(归并窗口为2个块)，当前面的块消耗完后，生成新的块，归并窗口后移，循环
+- 对每个chan排序输出的结果(chan)再归并输出为总排序结果
+
+### 问题：
+- 按照sendTime切块，qiekuan每次
--- twopc_sort.go
+++ twopc_sort.go
@@ -5,13 +5,19 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"context"
+	"fmt"
+	"sort"
+
+	"golang.org/x/time/rate"
 )
 
 var (
 	// number of client
 	clientNums = 2
 	// number of messages, simpify program implementation
-	messageNums   = 20
+	messageNums = 20
 	// assume dataStreaming has unlimited capacity
 	dataStreaming []chan data
 
@@ -29,6 +35,29 @@ type data struct {
 	prepare  int64
 	commit   int64
 	sendTime time.Time
+}
+
+type Datas []data
+
+func (ds Datas) Len() int {
+	return len(ds)
+}
+
+func (ds Datas) Less(i, j int) bool {
+	return ds[i].commit < ds[j].commit
+}
+
+func (ds Datas) Swap(i, j int) {
+	temp := ds[i]
+	ds[i] = ds[j]
+	ds[j] = temp
+}
+
+type dataBuf struct {
+	beginTime time.Time
+	vector    []data
+	cur       int
+	nextData  data
 }
 
 func init() {
@@ -59,7 +88,7 @@ func main() {
 
 	go func() {
 		defer wg.Done()
-		sort()
+		sortAndPrint()
 	}()
 	wg.Wait()
 }
@@ -70,8 +99,114 @@ func main() {
  * 2 sort commit kind of datas that are from multiple dataStreamings by commit ascending
  * and output them in the fastest way you can think
  */
-func sort() {
+func sortAndPrint() {
+	// sorted all chan result
+	sortAllDataStreaming := make(chan data, 1)
 
+	sortDataStreaming0 := make(chan data, 1)
+	sortDataStreaming1 := make(chan data, 1)
+	go func() {
+		sortDataStream(&dataStreaming[0], &sortDataStreaming0)
+	}()
+	go func() {
+		sortDataStream(&dataStreaming[1], &sortDataStreaming1)
+	}()
+	mergeDataStreaming(&sortDataStreaming0, &sortDataStreaming1, &sortAllDataStreaming)
+
+	l := rate.NewLimiter(20, 5)
+	c, _ := context.WithCancel(context.TODO())
+	fmt.Println(l.Limit(), l.Burst())
+	var i int64 = 0
+	for {
+		l.Wait(c)
+		oneData := <-sortAllDataStreaming
+		i++
+		fmt.Println(i, ", commit= ", oneData.commit, oneData.sendTime)
+	}
+}
+
+func getCommitData(dataChan *chan data) data {
+	for {
+		oneData := <-*dataChan
+		if oneData.commit != 0 {
+			return oneData
+		}
+	}
+}
+
+func sortDataStream(inputChan *chan data, sortedChan *chan data) {
+
+	oneData := getCommitData(inputChan)
+
+	databuf1 := makeAndSortDataBuf(&oneData, inputChan)
+	oneData = databuf1.nextData
+	databuf2 := makeAndSortDataBuf(&oneData, inputChan)
+	oneData = databuf2.nextData
+
+	for {
+		var minData data
+		if getMinData(&databuf1, &databuf2, &minData) {
+			*sortedChan <- minData
+		} else {
+			databuf1 = databuf2
+			databuf2 = makeAndSortDataBuf(&oneData, inputChan)
+			oneData = databuf2.nextData
+		}
+	}
+
+}
+
+func makeAndSortDataBuf(oneData *data, dataChan *chan data) dataBuf {
+	datas := dataBuf{
+		beginTime: oneData.sendTime,
+		vector:    []data{*oneData},
+		cur:       0,
+	}
+	for {
+		d1 := getCommitData(dataChan)
+		if d1.sendTime.Sub(datas.beginTime) <= time.Duration(maxSleepInterval)*time.Millisecond {
+			datas.vector = append(datas.vector, d1)
+		} else {
+			datas.nextData = d1
+			break
+		}
+	}
+	sort.Sort(Datas(datas.vector))
+	return datas
+}
+
+func getMinData(databuf1 *dataBuf, databuf2 *dataBuf, minData *data) bool {
+	if databuf1.cur < len(databuf1.vector) {
+
+		if databuf2.cur < len(databuf2.vector) {
+			if databuf2.vector[databuf2.cur].commit <= databuf1.vector[databuf1.cur].commit {
+				*minData = databuf2.vector[databuf2.cur]
+				databuf2.cur++
+				return true
+			}
+		}
+		*minData = databuf1.vector[databuf1.cur]
+		databuf1.cur++
+		return true
+	}
+	return false
+}
+
+//TODO use struct replace chan
+func mergeDataStreaming(dataChan0 *chan data, dataChan1 *chan data, sortedDataChan *chan data) {
+	go func() {
+		d0 := <-*dataChan0
+		d1 := <-*dataChan1
+		for {
+			if d0.commit <= d1.commit {
+				*sortedDataChan <- d0
+				d0 = <-*dataChan0
+			} else {
+				*sortedDataChan <- d1
+				d1 = <-*dataChan1
+			}
+		}
+	}()
 }
 
 /*
